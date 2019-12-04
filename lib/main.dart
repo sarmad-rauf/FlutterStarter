3bf318022477898cd14f0e9b557fa64a25e012b9
Rana Ranvijay Singh <ranaranvijaysingh9@gmail.com>
2019-12-04 09:34:40 +0530

    Bloc pattern.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'src/pics_demo/pic_app.dart';
 import 'src/login_demo/login_app.dart';
+import 'src/login_bloc_demo/login_bloc_app.dart';
 
 void main() {
-  runApp(LoginApp());
+  runApp(LoginBlocApp());
 }
