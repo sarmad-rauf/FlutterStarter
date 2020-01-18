d9b8f685cd46a0adbd7a179c7e4c140f59a51916
Rana Ranvijay Singh <rana.singh@globant.com>
2020-01-18 23:21:28 +0530

    Pull request test 4

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,25 +3,37 @@ pipeline {
 
     stages {
         stage('Code pull') {
+            when {
+                expression { return params.current_status == "closed" && params.merged == true }
+            }
             steps {
                 // Get some code from a GitHub repository
                 git 'https://github.com/RanaRanvijaySingh/FlutterStarter.git'
             }
         }
         stage('Build') {
+            when {
+                expression { return params.current_status == "closed" && params.merged == true }
+            }
             steps {
-                sh  '''
-                        #!/bin/sh
-                        flutter build apk --debug
-                    '''
+                sh '''
+            #!/bin/sh
+            flutter build apk --debug
+            '''
             }
         }
         stage('Test') {
+            when {
+                expression { return params.current_status == "closed" && params.merged == true }
+            }
             steps {
                 sh 'flutter test'
             }
         }
         stage('Publish') {
+            when {
+                expression { return params.current_status == "closed" && params.merged == true }
+            }
             steps {
                 appCenter apiToken: 'f51cf29ba6b2234a84c999bc37348db84624c615',
                         ownerName: 'ranaranvijaysingh9-gmail.com',
